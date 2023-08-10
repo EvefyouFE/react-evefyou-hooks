7c4b0e99ca10efa5664cb82a135a506fa85689cc
evef <eveforest@163.com>
2023-08-11 01:10:25 +0800

    exexternal peerDev from vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: EvefyouFE
+ * @Date: 2023-08-10 13:42:48
+ * @FilePath: \react-evefyou-hooks\vite.config.ts
+ * @Description: 
+ * Everyone is coming to the world i live in, as i am going to the world lives for you. 人人皆往我世界，我为世界中人人。
+ * Copyright (c) 2023 by EvefyouFE/evef, All Rights Reserved. 
+ */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import typescript from "@rollup/plugin-typescript";
@@ -29,7 +37,8 @@ export default defineConfig({
         sourceMap: true,
         declaration: true,
         outDir: 'dist'
-      })]
+      })],
+      external: ['react', 'react-dom', 'recoil','ramda', 'ahooks']
     }
   }
 })
